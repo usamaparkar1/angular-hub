b2311d43a23b677210977200a8176391d51e11ff
usamaparkar1 <usamaparkar1@gmail.com>
2025-09-07 14:08:13 +0530

    Update stock Price model to use timestamp and previous value

--- src/app/models/stock-prices.ts
+++ src/app/models/stock-prices.ts
@@ -1,11 +1,17 @@
 export class StockPrice implements IStockPrice {
     price: number;
+    symbol: string;
+    timestamp: Date;
 
     constructor (stockPrice: IStockPrice) {
         this.price = stockPrice.price;
+        this.symbol = stockPrice.symbol;
+        this.timestamp = stockPrice.timestamp;
     }
 }
 
 interface IStockPrice {
     price: number;
-}
+    symbol: string;
+    timestamp: Date;
+};
