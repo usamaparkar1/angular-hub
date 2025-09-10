b25850055578165ff598908fdd07e65228d4413b
usamaparkarorion <usamaparkar1@gmail.com>
2025-09-10 12:37:35 +0530

    Remove code from test comp

--- src/app/test/component/test.component.ts
+++ src/app/test/component/test.component.ts
@@ -1,8 +1,6 @@
-import { TestFormBuilderService } from '../utils/test-form-builder.service';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
-import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TestService } from '../service/test.service';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,49 +10,18 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule, ReactiveFormsModule]
 })
 
-export class TestComponent implements OnInit, AfterViewInit {
+export class TestComponent implements OnInit {
 
-  testForm: FormGroup = this._testFormBuilderService.createTestForm();
+  testForm!: FormGroup;
 
   constructor(
-    private _testService: TestService,
-    private _testFormBuilderService: TestFormBuilderService
-  ) {
-  }
+    private _testService: TestService
+  ) {}
 
-  ngOnInit(): void {
-    this._setupTestPage();
-  }
-
-  ngAfterViewInit(): void {
-    this._setupTestPageAfterViewInit();
-  }
-
-  private _setupTestPage() {
-    this._setupEventSubscriptionForSearchInput();
-  }
-
-  private _setupEventSubscriptionForSearchInput(): void {
-    this.testForm.get('search')?.valueChanges.pipe(
-      debounceTime(300),
-      distinctUntilChanged(),
-      switchMap((searchInput: any) =>
-        this._testService.avoidDuplicateHttpCalls(searchInput)
-      )
-    ).subscribe({
-      next: (result) => console.log('API Result:', result),
-      error: (err) => console.error('Error:', err),
-      complete: () => console.log('Completed')
-    });
-  }
-
-  private _setupTestPageAfterViewInit() {
-  }
+  ngOnInit(): void {}
 
   /** @description Example for shareReplay usage. */
   saveDuplicateHttpCallsInMemory(productId: string) {
     const response = this._testService.saveDuplicateHttpCallsInMemory(productId);
   }
 }
-
-// Migrate the form updates to a form component later on
